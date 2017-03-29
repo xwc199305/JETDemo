21badba6123039dd66caf880710507ff9e8add8a
wencxu-cn <649089504@qq.com>
2017-03-29 19:10:09 +0800

    begin myclub demo

---
+++ js/viewModels/Statistics.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Statistics module
+ */
+define(['ojs/ojcore', 'knockout','ojs/ojmodule'
+], function (oj, ko) {
+    /**
+     * The view model for the main content view template
+     */
+    function StatisticsContentViewModel() {
+        var self = this;
+    }
+    
+    return StatisticsContentViewModel;
+});
--- js/viewModels/home.js
+++ js/viewModels/home.js
@@ -7,14 +7,36 @@
 /**
  * home module
  */
-define(['ojs/ojcore', 'knockout'
+define(['ojs/ojcore', 'knockout', 'ojs/ojchart'
 ], function (oj, ko) {
     /**
      * The view model for the main content view template
      */
     function homeContentViewModel() {
         var self = this;
+        /* toggle button variables */
+        self.stackValue = ko.observable('off');
+        self.orientationValue = ko.observable('vertical');
+        /* chart data */
+        var barSeries = [{name: "Series 1", items: [42, 34]},
+            {name: "Series 2", items: [55, 30]},
+            {name: "Series 3", items: [36, 50]},
+            {name: "Series 4", items: [22, 46]},
+            {name: "Series 5", items: [22, 46]}];
+
+        var barGroups = ["Group A", "Group B"];
+        self.barSeriesValue = ko.observableArray(barSeries);
+        self.barGroupsValue = ko.observableArray(barGroups);
+        /* toggle buttons*/
+        self.stackOptions = [
+            {id: 'unstacked', label: 'unstacked', value: 'off', icon: 'oj-icon demo-bar-unstack'},
+            {id: 'stacked', label: 'stacked', value: 'on', icon: 'oj-icon demo-bar-stack'}
+        ];
+        self.orientationOptions = [
+            {id: 'vertical', label: 'vertical', value: 'vertical', icon: 'oj-icon demo-bar-vert'},
+            {id: 'horizontal', label: 'horizontal', value: 'horizontal', icon: 'oj-icon demo-bar-horiz'}
+        ];
     }
-    
+
     return homeContentViewModel;
 });
